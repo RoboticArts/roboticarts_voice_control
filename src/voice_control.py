ed2ee4e62818136aa6a81d00b75126bf7f84b850
Robert Vasquez Zavaleta <roboticarts1@gmail.com>
2020-07-17 00:48:46 +0200

    Added license, fixed bug in alias_timeout and in print function

--- src/voice_control.py
+++ src/voice_control.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 
 """
+Copyright (c) 2020 Robotic Arts Industries
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+"""
+ Author:  Robert Vasquez Zavaleta
+"""
+
+"""
 ---------------------------------------------------------------------
 Robotic Arts Industries
 All Rights Reserved 2017-2020
----------------------------------------------------------------------
-Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
-https://creativecommons.org/licenses/by-nc-sa/4.0/
 ---------------------------------------------------------------------
 Package: roboticarts_voice_control
 File: voice_control.py
@@ -24,7 +39,7 @@ Description:
             -   CMU Sphinx: offline, needs configuration to work well, only english
 
 ----------------------------------------------------------------------
-		Version: 0.0.1						| Last Modification: 19/04/2020
+		Version: 0.0.1						| Last Modification: 16/07/2020
 		Author:  Robert Vasquez Zavaleta
 		Contact: roboticarts1@gmail.com
 """
@@ -90,7 +105,7 @@ class RoboticartsVoiceControl:
         self.engine = param_dict.get(self.node_name + "/engine")
         self.alias = param_dict.get(self.node_name + "/alias")
         self.use_alias = param_dict.get(self.node_name + "/use_alias")
-        self.aliasTimeout = param_dict.get(self.node_name + "/aliasTimeout")
+        self.aliasTimeout = param_dict.get(self.node_name + "/alias_timeout")
         self.language = param_dict.get(self.node_name + "/language")
 
     def checkConfiguration(self):
@@ -260,7 +275,7 @@ class RoboticartsVoiceControl:
 
                     if keyword:
 
-                        rospy.loginfo("Keyword ", keyword, "recognized")
+                        rospy.loginfo("Keyword " + keyword + " recognized")
 
                         if(self.use_alias):
 
